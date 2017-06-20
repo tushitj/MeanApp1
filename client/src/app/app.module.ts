0b2845a4bc9160e4351c5a3c1882b23288334087
tushitjain <tushitjain@gmail.com>
2017-06-19 20:59:32 -0700

    Final commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import {HttpModule} from "@angular/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/contact.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by tushitjain on 6/19/17.
+ */
+export class Contact{
+  _id?: string;
+  first_name: string;
+  last_name: string;
+  phone: string;
+}
